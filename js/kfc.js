0b61e6f2f70ef6c05d65720103e7813f7a47f87c
LastOfLost <doglast157@gmail.com>
2024-01-05 20:53:42 +0200

    Initial commit

---
+++ js/kfc.js
@@ -0,0 +1,58 @@
+import {dataFood} from './dataFood.js';
+
+const DataForming = function({name, description, image, price}){
+
+    let result = document.createElement('div');
+    result.classList.add('item-area');
+
+    let item_title = document.createElement('p');
+    item_title.classList.add('item-title');
+    item_title.classList.add('item-property');
+    item_title.textContent = name;
+
+    let item_description = document.createElement('p');
+    item_description.classList.add('item-description');
+    item_description.textContent = description;
+
+    let item_image = document.createElement('div');
+    item_image.classList.add('item-image');
+    item_image.style.backgroundImage = (`url(${image})`);
+
+    let item_price = document.createElement('p');
+    item_price.classList.add('item-price');
+    item_price.textContent = `Buy by ${price}$`;
+
+    let item_buy_button = document.createElement('button');
+    item_buy_button.classList.add('item-buy-button');
+    item_buy_button.classList.add('center-content');
+    item_buy_button.appendChild(item_price);
+    
+    let item_line_1 = document.createElement('div');
+    item_line_1.classList.add('item-line-1');
+    
+    let item_line_2 = document.createElement('div');
+    item_line_2.classList.add('item-line-2');
+
+    result.appendChild(item_title);
+    result.appendChild(item_description);
+    result.appendChild(item_image);
+    result.appendChild(item_buy_button);
+    result.appendChild(item_line_1);
+    result.appendChild(item_line_2);
+
+    result["my_property_price"] = price;
+
+    return result;
+};
+
+const SortToMore = function(compareFunc, collection){
+    collection.sort((a,b)=>{a.my_property_price - b.my_property_price});
+};
+const SortToLess = function(compareFunc, collection){
+    collection.sort((a,b)=>{b.my_property_price - a.my_property_price});
+};
+
+let menu = document.getElementById('list-menu');
+dataFood.map(DataForming).forEach(element=>{
+    menu.appendChild(element);
+});
